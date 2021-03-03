34406cb99c27f9f2577f69dcd0dc28463acca4f5
PlaidMoose <79758282+PlaidMoose@users.noreply.github.com>
2021-03-03 00:20:47 -0600

    Added Migrations

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -1,8 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const constant_1 = require("./constant");
 const Post_1 = require("./entities/Post");
+const path_1 = __importDefault(require("path"));
+console.log("dirname: ", __dirname);
 exports.default = {
+    migrations: {
+        path: path_1.default.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     entities: [Post_1.Post],
     dbName: 'long-project',
     password: process.env.POSTRES_PWD,
--- scr/mikro-orm.config.ts
+++ scr/mikro-orm.config.ts
@@ -2,9 +2,14 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constant";
 import { Post } from "./entities/Post";
+import path from 'path';
 
-
+console.log("dirname: ", __dirname);
 export default{
+        migrations: {
+            path: path.join(__dirname, './migrations'), // path to the folder with migrations
+            pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+        },
         //Will correspond to all our database tables
         entities: [Post],
         //Connects to the Database
@@ -16,4 +21,5 @@ export default{
         //takes prod from constant and makes it easier to type
         debug: !__prod__,
     } as Parameters<typeof MikroORM.init>[0];
+//Let's us pass more into default
 
